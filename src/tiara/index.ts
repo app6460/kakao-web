0428cd281db5e881b97a20047dace7d3901fdff9
app6460 <app6460p@gmail.com>
2023-11-11 20:23:32 +0900

    refactor: `tiara-factory.ts`

--- src/tiara/index.ts
+++ src/tiara/index.ts
@@ -5,3 +5,49 @@
  */
 
 export * from './tiara-factory';
+
+export interface ITrackObject {
+    sdk: ISdk
+    env: IEnv
+    common: ICommon
+    etc: IEtc
+    action: IAction
+}
+
+export interface ISdk {
+    type: string
+    version: string
+}
+
+export interface IEnv {
+    screen: string
+    tz: string
+    cke: string
+}
+
+export interface ICommon {
+    svcdomain: string
+    deployment: string
+    url: string
+    section: string
+    page: string
+}
+
+export interface IEtc {
+    client_info: IClientInfo
+}
+
+export interface IClientInfo {
+    tuid: string
+    tsid: string
+    uuid: string
+    suid: string
+    isuid: string
+    client_timestamp: number
+}
+
+export interface IAction {
+    type: string
+    name: string
+    kind: string
+}
--- src/tiara/tiara-factory.ts
+++ src/tiara/tiara-factory.ts
@@ -4,22 +4,33 @@
  * Copyright (c) app6460. Licensed under the MIT License.
  */
 
+import { ITrackObject } from './index';
+
 export interface ITiaraConfig {
     referer: string;
     section: string;
     page: string;
 }
 
-export class TiaraFactory {
-    // eslint-disable-next-line max-len
-    private static seed = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+export namespace TiaraFactory {
+    export const seed = [
+        '0', '1', '2', '3', '4', '5',
+        '6', '7', '8', '9', 'a', 'b',
+        'c', 'd', 'e', 'f', 'g', 'h',
+        'i', 'j', 'k', 'l', 'm', 'n',
+        'o', 'p', 'q', 'r', 's', 't',
+        'u', 'v', 'w', 'x', 'y', 'z',
+        'A', 'B', 'C', 'D', 'E', 'F',
+        'G', 'H', 'I', 'J', 'K', 'L',
+        'M', 'N', 'O', 'P', 'Q', 'R',
+        'S', 'T', 'U', 'V', 'W', 'X',
+        'Y', 'Z',
+    ];
 
-    private constructor() {}
-
-    public static generateTiara(config: ITiaraConfig) {
-        const tuid = this.generateRandomUUIDWithDateTime();
-        const uuid = this.generateRandomUUIDWithDateNumber();
-        const isuid = this.generateRandomUUIDWithDateNumber();
+    export function generateTiara(config: ITiaraConfig): ITrackObject {
+        const tuid = generateRandomUUIDWithDateTime();
+        const uuid = generateRandomUUIDWithDateNumber();
+        const isuid = generateRandomUUIDWithDateNumber();
 
         return {
             sdk: {
@@ -35,7 +46,6 @@ export class TiaraFactory {
                 svcdomain: 'accounts.kakao.com',
                 deployment: 'production',
                 url: 'https://accounts.kakao.com/login/',
-                referrer: config.referer,
                 section: config.section,
                 page: config.page,
             },
@@ -57,21 +67,21 @@ export class TiaraFactory {
         };
     }
 
-    public static generateRandomUUIDWithDateTime() {
-        return `w-${this.shortenID(12)}_${this.currentTimeStamp()}`;
+    export function generateRandomUUIDWithDateTime() {
+        return `w-${shortenID(12)}_${currentTimeStamp()}`;
     }
 
-    public static generateRandomUUIDWithDateNumber() {
-        return `w-${this.shortenID(12)}_${this.currentTimeStamp().substring(0, 6)}${this.randomNumericString(9)}`;
+    export function generateRandomUUIDWithDateNumber() {
+        return `w-${shortenID(12)}_${currentTimeStamp().substring(0, 6)}${randomNumericString(9)}`;
     }
 
-    public static currentTimeStamp() {
+    export function currentTimeStamp() {
         const date = new Date();
         date.setHours(date.getHours() + 9);
         return date.toISOString().replace(/T|Z|-|:|\./g, '').substring(2);
     }
 
-    public static randomNumericString(length: number) {
+    export function randomNumericString(length: number) {
         let result = '';
         for (let i = 0; i < length; i++) {
             result += Math.floor(Math.random() * 10).toString();
@@ -79,10 +89,10 @@ export class TiaraFactory {
         return result;
     }
 
-    public static shortenID(length: number) {
+    export function shortenID(length: number) {
         let result = '';
         for (let i = 0; i < length; i++) {
-            result += this.seed[Math.floor(Math.random() * this.seed.length)];
+            result += seed[Math.floor(Math.random() * seed.length)];
         }
         return result;
     }
